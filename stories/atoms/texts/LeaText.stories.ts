456982521235b008999688116bf425200442cc51
Brian Muldown <brian@muldown.com>
2020-06-24 17:06:09 +0200

    Fix component name

--- stories/atoms/texts/LeaText.stories.ts
+++ stories/atoms/texts/LeaText.stories.ts
@@ -1,8 +1,8 @@
 import { addDecorator, storiesOf } from "@storybook/vue";
 // @ts-ignore
-import GafiText from "@/components/atoms/texts/LeaText.vue";
+import LeaText from "@/components/atoms/texts/LeaText.vue";
 
 storiesOf("Atoms/Texts/RegularText", module).add("default", () => ({
-  components: { GafiText },
-  template: "<gafi-text>Example text</gafi-text>"
+  components: { LeaText },
+  template: "<lea-text>Example text</lea-text>"
 }));
